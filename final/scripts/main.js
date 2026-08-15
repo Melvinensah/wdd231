06a57589b04255d872290e3bc78550d0a2f46eb1
Melvinensah <melvinbunduka@gmail.com>
2026-08-15 20:51:04 +0000

    updated

--- final/scripts/main.js
+++ final/scripts/main.js
@@ -8,6 +8,21 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
+  function displayRequiredFooterData() {
+    const currentYearSpan = document.getElementById("currentyear");
+    const lastModifiedParagraph = document.getElementById("lastModified");
+
+    if (currentYearSpan) {
+        currentYearSpan.textContent = new Date().getFullYear();
+    }
+    
+    if (lastModifiedParagraph) {
+        lastModifiedParagraph.textContent = `Last Modification: ${document.lastModified}`;
+    }
+  }
+
+  displayRequiredFooterData();
+
   const currentLocation = location.pathname.split('/').pop() || 'index.html';
   document.querySelectorAll('nav ul li a').forEach(link => {
     if (link.getAttribute('href') === currentLocation) {
